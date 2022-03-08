84200c5b99fd39135e4a630b3f7732cc89401e1f
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-03-08 08:16:15 -0300

    Update gerador.php

--- gerador.php
+++ gerador.php
@@ -1,29 +1,40 @@
 <?php
+
 date_default_timezone_set("America/Fortaleza"); 
-$data = date('d-m-Y H:i:s');
-$pro = rand();
+setlocale( LC_ALL, 'pt_BR.utf-8');
+$data = date('d.m.Y - H:i:s');
+$pro = rand(1, 99999);
+$ped = $_POST["pedido"];
+$end = $_POST["end"];
+
 
 
   // CRIAÇÃO DA IMAGEM COM OS PARÂMETROS DO TAMANHO
-$image = imagecreate(300, 500);
+$image = imagecreate(350, 580);
   
 // SELEÇÃO DA COR DE FUNDO DA IMAGEM
 $background_color = imagecolorallocate($image, 255, 255, 255);
   
 // SELEÇÃO DA COR DO TEXTO
 $text_color = imagecolorallocate($image, 0, 0, 0);
+
+
   
 // SELEÇÃO DA POSIÇÃO DO TEXTO E DO CONTEÚDO
-imagestring($image, 5, 45, 80,  "PROTOCOLO DE ATENDIMENTO", $text_color);
-imagestring($image, 3, 45, 100,  $pro, $text_color);
-imagestring($image, 3, 45, 120, "$data", $text_color);
-imagestring($image, 3, 45, 420, "By: Cristiano Ferreira", $text_color);
+imagestring($image, 5, 60, 40,  "PEDIDO - NOME DO ESTABELECIMENTO", $text_color);
+imagestring($image, 3, 20, 80,  "ID: $pro", $text_color);            //Número de protocolo
+imagestring($image, 3, 20, 95, "DATA: $data", $text_color);          //data e hora do pedido
+imagestring($image, 3, 20, 150, "PEDIDO: $ped", $text_color);    //pedido
+imagestring($image, 3, 20, 170, "ENDERECO: $end", $text_color); //quantidade
+
+imagestring($image, 3, 45, 480, "By: Cristiano Ferreira", $text_color);
 
   
+
 header("Content-Type: image/png");
-  
-imagepng($image);
-imagedestroy($image);
 
+imagepng($image);
+
+imagedestroy($image);
 
 ?>
