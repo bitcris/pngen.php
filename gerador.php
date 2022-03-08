121000c796ceedaef137a44199b5aa3dc395b52a
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-03-08 08:17:18 -0300

    Update gerador.php

--- gerador.php
+++ gerador.php
@@ -22,10 +22,10 @@ $text_color = imagecolorallocate($image, 0, 0, 0);
   
 // SELEÇÃO DA POSIÇÃO DO TEXTO E DO CONTEÚDO
 imagestring($image, 5, 60, 40,  "PEDIDO - NOME DO ESTABELECIMENTO", $text_color);
-imagestring($image, 3, 20, 80,  "ID: $pro", $text_color);            //Número de protocolo
+imagestring($image, 3, 20, 80,  "ID: $pro", $text_color);             //Número de protocolo
 imagestring($image, 3, 20, 95, "DATA: $data", $text_color);          //data e hora do pedido
-imagestring($image, 3, 20, 150, "PEDIDO: $ped", $text_color);    //pedido
-imagestring($image, 3, 20, 170, "ENDERECO: $end", $text_color); //quantidade
+imagestring($image, 3, 20, 150, "PEDIDO: $ped", $text_color);       //pedido
+imagestring($image, 3, 20, 170, "ENDERECO: $end", $text_color);    //endereço
 
 imagestring($image, 3, 45, 480, "By: Cristiano Ferreira", $text_color);
 
